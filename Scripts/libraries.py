721ccee30d8f5a9e65beec024395e3582c409e4c
Roberto Bastida Luna <robertobastidaluna@localhost.localdomain>
2021-04-17 12:43:18 -0500

    Proyecto

---
+++ Scripts/libraries.py
@@ -0,0 +1,21 @@
+import ipyvolume as ipv
+from ipywidgets import interact, interact_manual, widgets
+import pylab as pl
+from collections import Counter
+
+# Biblioteca para estandarización de los datos
+from numpy import asarray
+from sklearn.preprocessing import MinMaxScaler
+
+# Bibliotecas desarrolladas para el proyecto
+# Juego del Caos y Dimensión Fractal
+import os
+import sys
+#sys.path.insert(0, "../../DROGC-Bastida/Scripts/")
+
+# Bilbioteca desarrollada para el presente proyecto
+import SFI, DNA, fractal_dimension
+from DNA import *
+from SFI import *
+
+#from fractal_dimension import *
